e579f90f8aae824375768ea271ed24bad746f420
Darshan Patel <darshanpatel25894@gmail.com>
2017-10-05 20:01:48 -0400

    (Hotel) Added Hotel class

---
+++ src/Hotel.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Hotel {
+    String hotel_name;
+    int nightly_rate;
+    String promo_txt;
+    int deal_value;
+    String deal_type;
+    Date start_date;
+    Date end_date;
+
+}
