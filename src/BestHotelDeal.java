36b5508a58843aae946699e6dd55afa2eae2b745
Darshan Patel <darshanpatel25894@gmail.com>
2017-10-06 00:58:06 -0400

    (main) Finished the first solution of the program

--- src/BestHotelDeal.java
+++ src/BestHotelDeal.java
@@ -1,9 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -13,15 +12,19 @@ public class BestHotelDeal {
 
         /* STEP 1 : PARSE 'DEALS.CSV' AND CREATE HOTEL DATABASE */
 
-        HotelDB database;
+        HotelDB hotelDB = new HotelDB();
         try {
-            database = createDatabase("");
+            hotelDB = createDatabase(args[0]);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             // print Instructions
         }
 
 
+        /* STEP 2 : FIND THE BEST HOTEL DEAL FROM THE PROVIDED INPUT */
+
+        String bestDeal = findBestHotelDeal(hotelDB, args[1], args[2], Integer.parseInt(args[3]));
+        System.out.println(bestDeal);
 
 
     }
@@ -29,7 +32,7 @@ public class BestHotelDeal {
     public static HotelDB createDatabase(String path) throws FileNotFoundException{
 
         HotelDB database = new HotelDB();
-        Scanner scanner = new Scanner(new File("./src/deals.csv"));
+        Scanner scanner = new Scanner(new File("./deals.csv"));
 
         scanner.useDelimiter("[\\n,]");
 
@@ -39,7 +42,7 @@ public class BestHotelDeal {
                 // using an object array to hold values of different data types
                 String[] input = new String[7];
 
-                for(int i = 0; i < 7; i++) {
+                for (int i = 0; i < 7; i++) {
                     if(scanner.hasNext()) input[i] = scanner.next();
                 }
 
@@ -56,5 +59,38 @@ public class BestHotelDeal {
         }
 
         return database;
+    }
+
+    public static String findBestHotelDeal(HotelDB hotelDB, String hotel_name, String date, int num_nights) {
+
+        double min = Integer.MAX_VALUE;
+        Hotel bestDeal = null;
+
+        if (hotelDB.database.containsKey(hotel_name)) {
+            ArrayList<Hotel> hotel_deals = hotelDB.database.get(hotel_name);
+
+
+            loop: for (int i = 0; i < hotel_deals.size(); i++) {
+                Hotel currHotel = hotel_deals.get(i);
+
+                if (currHotel.deal_type.equals("rebate") && num_nights >= 3) continue loop;
+                if (currHotel.deal_type.equals("rebate_3plus") && num_nights < 3) continue loop;
+
+
+                if (currHotel.isDateRangeWithinDealRange(date, num_nights)) {
+                    double score = currHotel.evaluateDeal(num_nights);
+                    if (score < min) {
+                        bestDeal = currHotel;
+                        min = score;
+                    }
+                }
+            }
+        }
+
+        if (bestDeal == null) {
+            return "no deal available";
+        } else {
+            return bestDeal.promo_txt;
+        }
     }
 }
